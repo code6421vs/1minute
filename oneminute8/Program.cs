d0f0c9c0250277c5f50bd20117eaa43a1066194d
code6421vs <code6421vs@gmail.com>
2019-01-18 11:12:27 +0800

    'initiali'

---
+++ oneminute8/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oneminute8
+{
+    class Program
+    {
+        class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+
+        static IEnumerable<Person> FindPerson(IEnumerable<Person> source, string name, int? age = null)
+        {
+            if (name != null)
+                source = source.Where(a => a.Name == name);
+            if (age != null)
+                source = source.Where(a => a.Age == age);
+            return source;
+        }
+
+        static IEnumerable<Person> FindPerson2(IEnumerable<Person> source, string name, int? age = null)
+        {
+            IEnumerable<Person> c(object v) =>
+                        v == null ? source : null;
+            source = c(name) ?? source.Where(a => a.Name == name);
+            source = c(age) ?? source.Where(a => a.Age == age);
+            return source;
+        }
+
+        static void Main(string[] args)
+        {
+            var list = new Person[]
+            {
+                new Person() { Name = "code6421", Age = 12 },
+                new Person() {Name = "mary", Age = 16},
+                new Person() {Name = "ark", Age = 12},
+                new Person() {Name = "jerry", Age = 14},
+            };
+            
+            foreach (var item in FindPerson2(list, "code6421"))
+                Console.WriteLine(item.Name);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ oneminute9/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oneminute9
+{
+    class Program
+    {
+        class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+
+        static Expression<Func<T, bool>> DynamicWhere<T>(IDictionary<string, object> conditions)
+        {
+            ParameterExpression pe = Expression.Parameter(typeof(T), "a");
+            BinaryExpression be = null;
+            foreach (var item in conditions)
+            {
+                MemberExpression me = Expression.Property(pe, item.Key);
+                ConstantExpression ce = Expression.Constant(item.Value,
+                    typeof(T).GetProperty(item.Key).PropertyType);
+                if (be == null)
+                    be = Expression.Equal(me, ce);
+                else
+                    be = BinaryExpression.And(be, Expression.Equal(me, ce));
+            }
+            return Expression.Lambda<Func<T, bool>>(
+                be, new[] { pe });
+        }
+
+        static void Main(string[] args)
+        {
+            var list = new Person[]
+            {
+                new Person() { Name = "code6421", Age = 12 },
+                new Person() {Name = "mary", Age = 16},
+                new Person() {Name = "ark", Age = 12},
+                new Person() {Name = "jerry", Age = 14},
+            };
+
+            var expr = DynamicWhere<Person>(new Dictionary<string, object>
+            {
+                {"Name", "code6421" },
+                {"Age", 12 },
+            });
+
+            foreach (var item in list.AsQueryable().Where(expr))
+                Console.WriteLine(item.Name);
+            Console.ReadLine();
+        }
+    }
+}
