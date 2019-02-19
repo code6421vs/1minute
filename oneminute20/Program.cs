d7af146cff202fc99c0acb546477342d565fc3af
Jeffray Huang <code6421p@msn.com>
2019-02-19 13:45:29 +0800

    20

---
+++ oneminute20/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oneminute20
+{
+    class Program
+    {
+        class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public int Credit { get; set; }
+        }
+
+        struct PersonStruct
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public int Credit { get; set; }
+        }
+
+        static string TestPassByClass(Person p)
+        {
+            p.Age = p.Age + DateTime.Now.Minute;
+            return p.Name + p.Age + p.Credit;
+        }
+
+        static string TestPassByStruct(PersonStruct p)
+        {
+            p.Age = p.Age + DateTime.Now.Minute;
+            return p.Name + p.Age + p.Credit;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
