a0f9db71538403f6d465714308957879ee06adc4
Jeffray Huang <code6421p@msn.com>
2019-02-20 15:44:29 +0800

    update 20.

--- oneminute20/Program.cs
+++ oneminute20/Program.cs
@@ -33,9 +33,91 @@ namespace oneminute20
             return p.Name + p.Age + p.Credit;
         }
 
+        static void Benchmark(Action func, string title)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            func();
+            sw.Stop();
+            Console.WriteLine($"{title} elapsed {sw.ElapsedMilliseconds} ms");
+        }
+
         static void Main(string[] args)
         {
+            int loopSize = 10000000;
+            Benchmark(() =>
+            {
+                var p = new Person()
+                {
+                    Name = "code6421",
+                    Age = 12,
+                    Credit = 120000
+                };
+                for (var i = 0; i < loopSize; i++)
+                    TestPassByClass(p);
+            }, "By Class");
 
+            Benchmark(() =>
+            {
+                var p = new PersonStruct()
+                {
+                    Name = "code6421",
+                    Age = 12,
+                    Credit = 120000
+                };
+                for (var i = 0; i < loopSize; i++)
+                    TestPassByStruct(p);
+            }, "By Struct");
+
+            Benchmark(() =>
+            {
+                var p = new Person()
+                {
+                    Name = "code6421",
+                    Age = 12,
+                    Credit = 120000
+                };
+                for (var i = 0; i < loopSize; i++)
+                    TestPassByClass(p);
+            }, "By Class");
+
+            Benchmark(() =>
+            {
+                var p = new PersonStruct()
+                {
+                    Name = "code6421",
+                    Age = 12,
+                    Credit = 120000
+                };
+                for (var i = 0; i < loopSize; i++)
+                    TestPassByStruct(p);
+            }, "By Struct");
+
+            Benchmark(() =>
+            {
+                var p = new Person()
+                {
+                    Name = "code6421",
+                    Age = 12,
+                    Credit = 120000
+                };
+                for (var i = 0; i < loopSize; i++)
+                    TestPassByClass(p);
+            }, "By Class");
+
+            Benchmark(() =>
+            {
+                var p = new PersonStruct()
+                {
+                    Name = "code6421",
+                    Age = 12,
+                    Credit = 120000
+                };
+                for (var i = 0; i < loopSize; i++)
+                    TestPassByStruct(p);
+            }, "By Struct");
+
+            Console.ReadLine();
         }
     }
 }
